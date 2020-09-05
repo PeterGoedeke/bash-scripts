88b3f4e02ff1940fbf1c2dbd0f240a6816c9c907
Peter Goedeke <peterbgoedeke@gmail.com>
2020-09-05 14:11:29 +1200

    feat: added arguments to share file

--- share.sh
+++ share.sh
@@ -1,19 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
+path="./shared"
+port=8000
 
-if [[ $# -eq 0 ]]; then
-	echo "Usage: <filepath>"
-	exit 1;
+if ! command -v xclip &> /dev/null; then
+	echo "This script requires python3 to be installed."
+	exit
 fi
-
-mkdir -p /home/peter/Documents/shared 2> /dev/null
-
-cp $1 /home/peter/Documents/shared/$(basename $1)
-
-cd /home/peter/Documents/shared
 
 publicIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 localIP=$(hostname -I)
-echo "Server hosted publicly at $publicIP:8000"
-echo "Server hosted locally at $localIP:8000" 
-python3 -m http.server 1> /dev/null
-echo $publicIP:8000 | $(xclip -selection clipboard)
+copyIP=$publicIP
+
+while getopts ":p:dl" opt; do
+	case ${opt} in
+		d ) rm -rf $path
+		  ;;
+		p ) port=$OPTARG
+		  ;;
+		l ) copyIP=$localIP
+		  ;;
+		: ) echo "This option requires an argument"
+		  ;;
+		\? ) echo "Usage: cmd [-d] [-p] [-l] SOURCE"
+
+	esac
+done
+shift $((OPTIND -1))
+
+if [[ $# -eq 0 ]]; then
+	echo "Usage: [OPTIONS] SOURCE"
+	exit 1;
+fi
+
+mkdir -p $path 2> /dev/null
+
+cp "$1" "$path/$(basename "$1")"
+
+cd $path || exit
+
+echo "Server hosted publicly at $publicIP:$port"
+echo "Server hosted locally at $localIP:$port" 
+
+python3 -m http.server "$port" 1> /dev/null &
+
+if ! command -v xclip &> /dev/null; then
+	exit
+fi
+echo "$copyIP:$port"
+echo "$copyIP:$port" | $(xclip -selection clipboard)
